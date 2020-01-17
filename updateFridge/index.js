73972e10dbbb8aa91e78aaf76c6687c91f7203d9
Ahmed Elshamekh <59749029+ahmed-elshamekh@users.noreply.github.com>
2020-01-17 17:45:10 -0600

    Create index.js

---
+++ updateFridge/index.js
@@ -0,0 +1,81 @@
+const aws = require('aws-sdk');
+aws.config.update({region: "us-east-1"});
+
+const db = new aws.DynamoDB({apiVersion: "2012-10-08"});
+
+// The document client simplifies working with items in Amazon DynamoDB by abstracting away the notion of attribute values.
+// This abstraction annotates native JavaScript types supplied as input parameters, as well as converts annotated response data to native JavaScript types.
+const docClient = new aws.DynamoDB.DocumentClient({region: "us-east-1"});
+    
+exports.handler = async (event, context) => {
+    var responseBody;
+    var responseCode;
+
+    // Get the data to be used in the POST request.
+    // Construct the JavaScript object described by the string
+    var {fridgeSN, itemName, count} = JSON.parse(event.body);
+    
+    if (itemName === "soda" && count > 12) {
+        responseBody = 'You cannot have more than 12 cans of soda in your fridge';
+        responseCode = 200;
+    }
+    else {
+        
+        // Check if the fridge you want to update exists.
+        var checkFridgeExist = {
+            TableName: "Fridges",
+            Key:
+            {
+                fridgeSN: fridgeSN
+            }
+        }
+        
+        var result = await docClient.get(checkFridgeExist).promise();
+        
+        // Convert the response to a JSON string.
+        responseBody = JSON.stringify(result.Item);
+        
+        if (!responseBody) {
+            responseBody = 'The specified fridge cannot be found';
+            responseCode = 404;
+        }
+        
+        else {
+            var params = {
+            TableName: "Fridges",
+                Key: {
+                    fridgeSN: fridgeSN
+                },
+                UpdateExpression: "set #MyVariable = :count",
+                ExpressionAttributeNames: {
+                    "#MyVariable": itemName
+                },
+                ExpressionAttributeValues: {
+                    ":count": count
+                },
+                ReturnValues:"UPDATED_NEW"
+            }
+    
+            try {
+                const data = await docClient.update(params).promise();
+                responseBody = 'Changes have been applied to your fridge';
+                responseCode = 200;
+            }
+            catch (err) {
+                responseBody = 'failed to add an item!';
+                responseCode = 404;
+            }
+        }
+    }
+    
+    // Create a response to be returned upon execution to indicate success or failure.
+    const response = {
+        statusCode: responseCode,
+        headers: {
+            "testHeaders": "test"
+        },
+        body: responseBody
+    }
+    
+    return response;
+}
